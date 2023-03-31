8f9c83e3a5dd3fa5f0bac9e5506ac2882fb9626f
Noah Black <64273297+NoahBlack012@users.noreply.github.com>
2023-03-31 16:26:45 -0400

    Bracket processing bug fix

--- helper.py
+++ helper.py
@@ -47,17 +47,29 @@ class _Node:
     def get_possible_true_combos(self) -> list[set[str]]:
         """
         Return all possible course combinations that make the given node return a truth value of true
+
+        >>> course = parse_course_requirements('CSC311H1|CSC411H1|STA314H1|ECE421H1|ROB313H1|CSCC11H3|CSC311H5')
+        >>> all(course.get_truth_value(i) is True for i in course.get_possible_true_combos())
+        True
         """
         if self._text == '':
             return [set()]
         elif is_course_format(self._text):
             return [{self._text}]
         else:
+            if self._left is not None:
+                left_rec = self._left.get_possible_true_combos()
+            else:
+                left_rec = []
+
+            if self._right is not None:
+                right_rec = self._right.get_possible_true_combos()
+            else:
+                right_rec = []
             if self._sep == '^':
                 # If the separator is and, both left and right must return true for the
                 # node to have a truth value of true
-                left_rec = self._left.get_possible_true_combos()
-                right_rec = self._right.get_possible_true_combos()
+
                 combos_so_far = []
 
                 # TODO: Optimize - Go from i to len(right_rec)??
@@ -67,10 +79,8 @@ class _Node:
                             combos_so_far.append(left_rec[i].union(right_rec[j]))
                 return combos_so_far
             else:
-                # If the separator is and, either the left and right nodes must return true for the
+                # If the separator is or, either the left or right nodes must return true for the
                 # node to have a truth value of true
-                left_rec = self._left.get_possible_true_combos()
-                right_rec = self._right.get_possible_true_combos()
                 return left_rec + right_rec
 
 
@@ -78,6 +88,7 @@ class RequirementTree:
     """
     Tree to represent a course requirements
 
+    _root: Root node for the tree (Node that represents the full course requirement text
     """
     _root: _Node
 
@@ -85,12 +96,16 @@ class RequirementTree:
         self._root = root_node
 
     def check_prereqs(self, user_courses: set[str]) -> bool:
+        """
+        Return whether a user has the requirements for the course
+        """
         return self._root.get_truth_value(user_courses)
 
     def get_all_possible_prereqs(self) -> list[set[str]]:
+        """
+        Get all possible combinations of courses that fufill the course requirements
+        """
         return self._root.get_possible_true_combos()
-
-
 
 
 def parse_course_requirements(requirements: str) -> _Node:
@@ -107,14 +122,15 @@ def parse_course_requirements(requirements: str) -> _Node:
     True
 
     """
+    # Make sure there is not a bracket at start and end of requirements string
+    while is_enclosed_brackets(requirements):
+        requirements = requirements[1:-1]
+
     if is_course_format(requirements) or requirements == '':
         # Base case - Requirements is just a single course or is an empty string
         return _Node(requirements)
     else:
         curr_node = _Node(requirements)
-        # Make sure there is not a bracket at start and end of requirements string
-        while is_enclosed_brackets(requirements):
-            requirements = requirements[1:-1]
 
         # Group courses by brackets with separators between brackets included in list
         # If a bracket/separator is encountered, reset curr_item?? If in brackets, ignore separators
@@ -190,11 +206,6 @@ def is_enclosed_brackets(s: str) -> bool:
         return True
 
 
-
-
-
-
-
 def is_course_format(s: str) -> bool:
     """
     Helper function - Return whether a string is in the format of a course
@@ -211,3 +222,8 @@ def is_course_format(s: str) -> bool:
         return True
     else:
         return False
+
+def merge_paths_multiple(paths: list[set[str]]):
+    """
+    Given the paths to multiple courses, merge them all into a single set of courses to take
+    """
